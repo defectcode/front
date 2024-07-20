c9a117bf0871293ae6a0c9043fc4888009a15cbd
Constantin <paladuta05@gmail.com>
2024-07-20 12:09:58 +0300

    add

--- src/app/components/Header/Header.jsx
+++ src/app/components/Header/Header.jsx
@@ -1,4 +1,5 @@
-'use client'
+
+'use client';
 import React, { useEffect, useState, useRef } from 'react';
 import Image from 'next/image';
 import Navbar from './components/Navbar';
@@ -13,7 +14,7 @@ const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY)
 export default function Header() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [showSupportInNavbar, setShowSupportInNavbar] = useState(false);
-  const backgroundRef = useRef(null);
+  const headerRef = useRef(null);
   const effectRan = useRef(false);
 
   const openModal = () => {
@@ -32,49 +33,65 @@ export default function Header() {
       document.documentElement.style.setProperty('--viewport-height', `${viewportHeight}px`);
     };
 
+    const handleScroll = () => {
+      const header = headerRef.current;
+      const navbar = document.querySelector('nav');
+
+      if (header && navbar) {
+        const headerBottom = header.getBoundingClientRect().bottom;
+        const navbarTop = navbar.getBoundingClientRect().top;
+
+        if (headerBottom <= navbarTop) {
+          setShowSupportInNavbar(true);
+        } else {
+          setShowSupportInNavbar(false);
+        }
+      }
+    };
+
     updateHeight();
     window.addEventListener('resize', updateHeight);
-
+    window.addEventListener('scroll', handleScroll);
 
     effectRan.current = true;
 
     return () => {
       window.removeEventListener('resize', updateHeight);
-      // window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener('scroll', handleScroll);
     };
   }, []);
 
   return (
     <div className="relative h-screen max-md:h-[var(--viewport-height)] w-auto text-white font-ekMukta overflow-hidden">
       <div
-        ref={backgroundRef}
+        ref={headerRef}
         className={`absolute inset-0 w-full h-full bg-cover bg-center bg-no-repeat max-md:w-auto ${isModalOpen ? 'bg-opacity-50 blur-sm' : ''} md:bg-[url('/imgs/background.svg')] bg-[url('/imgs/mobile.svg')] max-md:background-fixed`}
       ></div>
       <div className={`absolute bottom-0 w-full h-3/6 bg-gradient-to-t from-black/70 via-black/70 to-transparent ${isModalOpen ? 'bg-opacity-50 blur-sm' : ''}`}></div>
-      <div className="relative z-10 h-full overflow-auto">
-        <Navbar/>
+      <div className={`relative z-10 h-full overflow-auto ${isModalOpen ? 'blur-sm' : ''}`}>
+        <Navbar showSupportInNavbar={showSupportInNavbar} openModal={openModal} />
         <div className="max-w-[1300px] mx-auto h-full flex flex-col text-white max-2xl:p-4">
-          <div className="flex-grow flex flex-col justify-end max-2xl:p-2">
+          <div className="flex-grow flex flex-col justify-end max-2xl:p-2 header-content">
             <div className={`flex flex-col md:flex-row justify-between max-lg:flex-col max-lg:justify-center customStyles`}>
               <div className="flex flex-col mb-10 max-md:mb-0 items-center md:flex-row gap-3 max-lg:flex-col max-lg:justify-center">
                 <div className="md:hidden flex flex-col items-center justify-center mt-8">
-                  <Image src="/imgs/logo.svg" alt='logo' className='w-48 h-auto max-md:mb-5' width={128} height={128} />
-                  <h3 className="text-lg max-md:text-[15px] text-[#979797] font-avenirRoman">
+                  <Image src="/imgs/logo.svg" alt='logo' className='w-48 h-auto max-md:mb-4' width={128} height={128} />
+                  <h3 className="text-lg max-md:text-[14px] text-[#979797] font-avenirRoman">
                     Drama <span style={{ display: 'inline-block', width: '3px', height: '3px', backgroundColor: '#979797', borderRadius: '50%', margin: '0 5px', verticalAlign: 'middle' }}></span> Sci-Fi
                   </h3>
                 </div>
                 {!showSupportInNavbar && (
-                  <div className="md:flex md:items-center md:px-2 max-md:mb-4 font-avenirHeavy">
+                  <div className="md:flex md:items-center md:px-2 max-md:mb-4 font-avenirHeavy max-md:hidden">
                     <Support onClick={openModal} />
                   </div>
                 )}
                 <div className="space-y-1 md:flex md:flex-col md:items-start">
-                  <p className="text-[16px] max-lg:text-lg max-lg:text-[14px] max-lg:leading-5 max-md:text-center max-md:mb-4 text-[#CDCDCD] mx-0 font-ekMukta tracking-neg-3percent">
+                  <p className="text-lg max-lg:text-lg max-md:text-[15px] max-md:leading-5 max-lg:text-center max-md:mb-4 text-[#CDCDCD] mx-0 font-ekMukta tracking-neg-3percent">
                     In an ideal city without money, people face real human problems. Your support makes the series possible.
                   </p>
-                  <div className="flex items-center gap-5 text-[#979797] max-md:flex-col max-md:space-y-[12px]">
-                    <h3 className="text-[16px] max-md:text-base hidden md:block font-avenirRoman">Drama • Sci-Fi • 2024</h3>
-                    <div className="flex gap-5 max-lg:gap-2">
+                  <div className="flex items-center gap-5 text-[#979797] max-lg:flex-col max-lg:space-y-[12px]">
+                    <h3 className="text-lg max-md:text-base hidden md:block font-avenirRoman">Drama • Sci-Fi • 2024</h3>
+                    <div className="flex gap-4 max-md:gap-1 mb-1">
                       <Image src="/imgs/sony.svg" width={64} height={1} alt="sony" className="w-[46px] max-md:w-10 max-sm:w-9" />
                       <Image src="/imgs/netflix.svg" width={64} height={1} alt="netflix" className="w-[46px] max-md:w-10 max-sm:w-9" />
                     </div>
