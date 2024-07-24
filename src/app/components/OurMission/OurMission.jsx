fb7e3bcba70b75f0fb2214c56cdcae92c069f089
Constantin <paladuta05@gmail.com>
2024-07-24 16:20:13 +0300

    add commponent Our Mission

---
+++ src/app/components/OurMission/OurMission.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './OurMission.css'; 
+import GoalsSection from './components/GoalsSection'
+import Earth from './components/Earth'
+
+const OurMission = () => {
+    return (
+        <div>
+            <Earth/>
+            <GoalsSection/>
+        </div>
+    )
+}
+
+export default OurMission;
