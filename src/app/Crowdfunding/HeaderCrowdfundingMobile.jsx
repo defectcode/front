1412e2171569df58c5fe21abbed9e92b84633687
Constantin <paladuta05@gmail.com>
2024-09-10 17:47:27 +0300

    ref

--- src/app/Crowdfunding/HeaderCrowdfundingMobile.jsx
+++ src/app/Crowdfunding/HeaderCrowdfundingMobile.jsx
@@ -7,20 +7,20 @@ import { images } from './constants/carouselData';
 import styles from './style/Header.module.css';
 
 const HeaderCrowdfundingMobile = () => {
-    const currentData = images[0];
+    const currentData = images[0];  // Folosim primul element din lista de imagini
     const [isVideoVisible, setIsVideoVisible] = useState(false);
     const containerRef = useRef(null);  // Referință pentru componentă
 
     const handleScreenClick = () => {
-        setIsVideoVisible(true);
+        setIsVideoVisible(true); // Afișăm video player-ul
         document.body.classList.add('overflow-hidden'); // Previne scroll-ul în timpul redării video
     };
 
     const handleClose = () => {
-        setIsVideoVisible(false);
-        document.body.classList.remove('overflow-hidden');
+        setIsVideoVisible(false); // Închidem video player-ul
+        document.body.classList.remove('overflow-hidden'); // Permitem scroll-ul din nou
         if (document.fullscreenElement) {
-            document.exitFullscreen();
+            document.exitFullscreen(); // Iesim din modul fullscreen dacă este activ
         }
     };
 
@@ -30,7 +30,7 @@ const HeaderCrowdfundingMobile = () => {
                 <div 
                     className="relative" 
                     style={{ 
-                        width: '100%',  // Lățimea 100% din containerul părinte
+                        width: '100vw',  // Lățimea 100% din containerul părinte
                         aspectRatio: '9 / 16',  // Setează proporția 9:16 pentru a păstra corect raportul
                         overflow: 'hidden', // Ascunde orice conținut care depășește proporția
                         position: 'relative',  // Important pentru poziționarea corectă a butonului
@@ -39,8 +39,8 @@ const HeaderCrowdfundingMobile = () => {
                     <div 
                         className={`${styles.bgMobileHeader}`} 
                         style={{ 
-                            width: '100%', 
-                            height: '100%', 
+                            width: '100vw', 
+                            height: '100vh',  // Folosim dynamic viewport height pentru a evita variațiile
                             backgroundImage: `url(${currentData.imageUrl})`, 
                             backgroundSize: 'cover', // Păstrează proporțiile imaginii 
                             backgroundPosition: 'center', // Centrează imaginea
@@ -50,11 +50,14 @@ const HeaderCrowdfundingMobile = () => {
                     >
                         {/* Conținutul componentei Header */}
                         <div 
-                            className="max-w-screen-lg w-full flex flex-col gap-4 relative z-10 px-5"
+                            className={`${styles.headerContent} max-w-screen-lg w-full flex flex-col gap-4 relative z-10 px-5`}
                             style={{
                                 fontSize: 'calc(1rem + 0.7vw)',
-                                top: `75%`, // Poziționează la 69% de la începutul componentei
-                                transform: 'translateY(0)', // Eliminăm orice transformări suplimentare
+                                position: 'absolute',
+                                left: '0',
+                                width: '100%',
+                                textAlign: 'center',
+                                transform: 'translateY(0)',
                             }}
                         >
                             <Title title={currentData.title} description={currentData.description} />
@@ -63,24 +66,24 @@ const HeaderCrowdfundingMobile = () => {
 
                         {/* Gradient aplicat doar pe această componentă */}
                         <div
+                            className="-mb-10"
                             style={{
                                 position: 'absolute',
                                 bottom: 0,
-                                width: '100%',
-                                top: '41.76%',
-                                // bottom: '15px',
-                                backgroundImage: 'linear-gradient(to top, rgba(0, 0, 0, 0.8) 0%, rgba(0, 0, 0, 0) 100%)',
+                                width: '100vw',
+                                top: '50vh', // Începem gradientul de la această înălțime
+                                backgroundImage: 'linear-gradient(to top, rgba(0, 0, 0, 0.8) 60%, rgba(0, 0, 0, 0) 100%)',
                                 zIndex: 5, // Ridică gradientul deasupra fundalului
                             }}
                         />
-                        
+
                         {/* Buton de Play pentru versiunea mobilă */}
                         <button 
                             onClick={handleScreenClick} 
                             className="absolute flex items-center justify-center z-20 bg-transparent"
                             style={{
-                                top: '34.42%', // Amplasare la 34.42% din înălțimea imaginii de fundal
-                                left: '50%', // Centrează butonul pe orizontală
+                                top: '34.42vh', // Amplasare la 34.42% din înălțimea imaginii de fundal
+                                left: '50vw', // Centrează butonul pe orizontală
                                 transform: 'translate(-50%, -50%)', // Ajustare pentru centrare pe orizontală și verticală
                             }}
                         >
